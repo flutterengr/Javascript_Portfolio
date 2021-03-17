e9f6aa7f983bc0624f1fedca3463d4671d42cf76
gonzalodiascenzi <gonzalodiascenzi@gmail.com>
2021-03-17 18:35:58 -0300

    Listado de peliculas

---
+++ routes/movies.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+const moviesController = require("../controllers/moviesController")
+
+
+/* GET home page. */
+router.get('/', moviesController.list)
+
+
+module.exports = router;
