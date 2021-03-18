1ac9be7d8dbb7fa4717a3d7fd3608a94cbf2a7a7
gonzalodiascenzi <gonzalodiascenzi@gmail.com>
2021-03-18 04:01:10 -0300

    Listado de peliculas en la homepage

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var moviesRouter = require('./routes/movies');
 
@@ -20,9 +19,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+app.use('/', moviesRouter);
 app.use('/users', usersRouter);
-app.use('/movies', moviesRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
